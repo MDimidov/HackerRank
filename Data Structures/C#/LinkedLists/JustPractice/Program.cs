fa0752c4e83b5a2b9ea8fbdd20bb175e6aec1f32
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-04-27 12:26:40 +0300

    Sloved task 2

--- Data Structures/C#/LinkedLists/JustPractice/Program.cs
+++ Data Structures/C#/LinkedLists/JustPractice/Program.cs
@@ -5,8 +5,26 @@
 //    Преброява и отпечатва колко думи има в този текст.
 //    (Дума е всяка последователност от символи, разделени от интервали.)
 
-int reult = Console.ReadLine()!
-    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-    .Count();
+//int reult = Console.ReadLine()!
+//    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+//    .Count();
 
-Console.WriteLine(reult);
+//Console.WriteLine(reult);
+
+//Задача 2: Проверка за палиндром
+//Напиши програма, която:
+//    Чете един низ от входа.
+//    Проверява дали той е палиндром (чете се еднакво отляво надясно и отдясно наляво).
+//    Игнорира главни и малки букви.
+
+string input = Console.ReadLine()!.ToLower();
+var reversed = string.Concat(input.Reverse());
+
+if(input == reversed)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
