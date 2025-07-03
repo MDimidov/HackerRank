ea9ce9132bad49c38e10b7e6bc88184efc69a264
Mariqn Dimidov <mariqn.dimidov@gmail.com>
2025-07-03 12:32:09 +0300

    Solved for loop

--- Introduction/C++/ForLoop/ForLoop/ForLoop.cpp
+++ Introduction/C++/ForLoop/ForLoop/ForLoop.cpp
@@ -2,8 +2,39 @@
 //
 
 #include <iostream>
+#include <map>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a, b;
+
+    cin >> a >> b;
+
+    map<int, string> nums = {
+        {1, "one"},
+        {2, "two"},
+        {3, "three"},
+        {4, "four"},
+        {5, "five"},
+        {6, "six"},
+        {7, "seven"},
+        {8, "eight"},
+        {9, "nine"},
+    };
+
+    for (int i = a; i <= b; i++) {
+        if (nums.count(i)) {
+            cout << nums.at(i);
+        }
+        else if (i % 2 == 0) {
+            cout << "even";
+        }
+        else {
+            cout << "odd";
+        }
+
+        cout << endl;
+    }
 }
