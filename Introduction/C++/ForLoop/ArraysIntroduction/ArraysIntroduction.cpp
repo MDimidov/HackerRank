a5e6388a6b1d2ca8b19eb132034fc52961a450d3
Mariqn Dimidov <mariqn.dimidov@gmail.com>
2025-07-05 11:06:30 +0300

    Solved Arrays Introduction

--- Introduction/C++/ForLoop/ArraysIntroduction/ArraysIntroduction.cpp
+++ Introduction/C++/ForLoop/ArraysIntroduction/ArraysIntroduction.cpp
@@ -3,7 +3,20 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+	cin >> n;
+
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = n - 1; i >= 0; i--) {
+		cout << arr[i] << " ";
+	}
 }
